1f8d76f520ae144292be40d049bef15918291bdb
Ateriss <alexandralinares2729@gmail.com>
2023-04-07 00:31:00 -0500

    products update

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -36,13 +36,3 @@ function animarCarrusel() {
 animarCarrusel();
 
 
-const searchInput = document.getElementById("search");
-const searchContainer = document.querySelector(".header__search");
-
-searchInput.addEventListener("input", function() {
-  if (searchInput.value.length > 0) {
-    searchContainer.classList.add("with-text");
-  } else {
-    searchContainer.classList.remove("with-text");
-  }
-});
